eb0ed87f4818ce17f355ca2bae4e4da027350319
keunhyeong <rmsgudxx@gmail.com>
2025-04-05 15:41:12 +0900

    commit

--- 구현/1475 실버 5.py
+++ 구현/1475 실버 5.py
@@ -1,15 +1,12 @@
-#실패
-nums = input()
+
+n = input()
 count = [0 for _ in range(10)]
 
-for i in nums:
+for i in n:
     count[int(i)] += 1
 
-six_nine = (count[6] + count[9] + 1)//2
+six_nine = (count[6] + count[9] + 1) // 2
 count[6] = six_nine
 count[9] = 0
 
 print(max(count))
-
-
-888888
